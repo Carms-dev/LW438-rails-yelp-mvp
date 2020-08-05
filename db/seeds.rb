9aaf5bffffc1675c429f81b58921906622988618
Carms Ng <carmenhyng89@gmail.com>
2020-08-05 17:58:40 -0400

    migration & seed generated, controller and model created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+p 'seeding...'
+
+10.times do
+    resto = Restaurant.create!(
+        name: Faker::Restaurant.name,
+        address: Faker::Address.full_address,
+        phone: Faker::PhoneNumber.cell_phone,
+        # category: ['Chinese', 'Italian', 'Mexican'].sample
+        category: Faker::Restaurant.type
+    )
+    rand(1..5).times do
+        Review.create!(
+            content: Faker::Restaurant.review,
+            rating: rand(1..5),
+            restaurant: resto
+        )
+    end
+end
+
+p 'completed'
