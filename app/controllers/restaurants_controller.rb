005bc4de066db3262431a0e6eff47ea51ec17c96
Carms Ng <carmenhyng89@gmail.com>
2020-08-05 23:41:16 -0400

    restaurants and reviews mvc basics done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,41 @@
 class RestaurantsController < ApplicationController
+    before_action :find_restaurant, only: [:show]
+    before_action :set_new_restaurant
+
+    def index
+        @restaurants = Restaurant.all
+    end
+    
+    def show
+        @reviews = @restaurant.reviews
+        @new_review = Review.new
+    end
+
+    def new
+        
+    end
+    
+    def create
+        @restaurant = Restaurant.new(restaurant_params)
+        
+        if @restaurant.save
+            redirect_to restaurant_path(@restaurant)
+        else
+            # AJAx?
+        end
+    end
+
+    private
+
+    def find_restaurant
+        @restaurant = Restaurant.find(params[:id])
+    end
+
+    def set_new_restaurant
+        @new_restaurant = Restaurant.new
+    end
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,18 @@
 require 'faker'
 
+p 'destroying existing records'
+
+Review.destroy_all
+Restaurant.destroy_all
+
 p 'seeding...'
 
 10.times do
     resto = Restaurant.create!(
         name: Faker::Restaurant.name,
         address: Faker::Address.full_address,
-        phone: Faker::PhoneNumber.cell_phone,
-        # category: ['Chinese', 'Italian', 'Mexican'].sample
-        category: Faker::Restaurant.type
+        phone_number: Faker::PhoneNumber.cell_phone,
+        category: ["chinese", "italian", "japanese", "french", "belgian"].sample
     )
     rand(1..5).times do
         Review.create!(
